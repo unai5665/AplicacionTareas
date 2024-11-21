d4bab13ac2b79788078c2428af94058936fac270
unai <unaiperez5665@gmail.com>
2024-11-21 13:38:39 +0000

    eliminada importacion duplicada

--- app/src/main/java/org/iesharia/aplicacintareas/data/DataBase.kt
+++ app/src/main/java/org/iesharia/aplicacintareas/data/DataBase.kt
@@ -1,7 +1,6 @@
 package org.iesharia.aplicacintareas.data
 
 import android.content.Context
-import androidx.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
