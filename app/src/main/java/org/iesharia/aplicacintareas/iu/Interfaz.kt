ea2d6787c1501e1144df400e51613f1a4205ecbb
unai5665 <151141045+unai5665@users.noreply.github.com>
2024-11-22 10:24:24 +0000

    añadidas importaciones a Interfaz.kt

--- app/src/main/java/org/iesharia/aplicacintareas/iu/Interfaz.kt
+++ app/src/main/java/org/iesharia/aplicacintareas/iu/Interfaz.kt
@@ -1,2 +1,26 @@
 package org.iesharia.aplicacintareas.iu
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.TextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import org.iesharia.aplicaciontareas.data.Tarea
+import org.iesharia.aplicaciontareas.data.TareaDao.TareaConTipo
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import org.iesharia.aplicaciontareas.model.TareasRepository
 
